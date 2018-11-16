99f104fb5d83a6762b0cf0f9db19df12eea16579
hmuar <1871299+hmuar@users.noreply.github.com>
2018-11-16 08:15:09 -0500

    reduce complexity

--- main.go
+++ main.go
@@ -1,53 +1,65 @@
 package main
 
 import (
-  "fmt"
-  "bufio"
-  "os"
-  "./board"
-  "strconv"
+	"bufio"
+	"fmt"
+	"github.com/hmuar/go-tic-tac-toe/board"
+	"os"
+	"strconv"
 )
 
 func playerInput(prompt string) int {
-  for {
-    fmt.Printf(prompt)
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    playerMove, err := strconv.ParseInt(scanner.Text(), 10, 32)
-    if err != nil {
-      fmt.Println("Invalid move")
-    } else {
-      return int(playerMove)
-    }
-  }
+	for {
+		fmt.Printf(prompt)
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		playerMove, err := strconv.ParseInt(scanner.Text(), 10, 32)
+		if err != nil {
+			fmt.Println("Invalid move")
+		} else {
+			return int(playerMove)
+		}
+	}
 }
 
 func doPlayerMove(player int) (int, int) {
-  fmt.Printf("-- Player %v turn --\n", player)
-  row := playerInput("row:")
-  col := playerInput("col:")
-  return row, col
+	fmt.Printf("-- Player %v turn --\n", player)
+	row := playerInput("row:")
+	col := playerInput("col:")
+	return row, col
+}
+
+func endGame(b *board.Board, winningPlayer board.Player) {
+	if winningPlayer == board.Player(-1) {
+		fmt.Println("Game over, it was a draw :D")
+	} else {
+		fmt.Printf("Player %v wins! :D\n", winningPlayer)
+	}
+}
+
+func gameStep(b *board.Board, player int) int {
+	row, col := doPlayerMove(player)
+	b.Mark(row, col, board.Player(player))
+	displayBoard(b)
+	return (player + 1) % 2
+}
+
+func displayBoard(b *board.Board) {
+	fmt.Println(b)
 }
 
 func main() {
-  fmt.Println("Tic Tac Toe :D\n")
-  b := board.NewBoard()
-  fmt.Println(b)
-  gameOver := false
-  player := 0
-  for !gameOver {
-    row, col := doPlayerMove(player)
-    b.Mark(row, col, board.Player(player))
-    player = (player + 1) % 2
-    fmt.Println(b)
-    gameOver, winningPlayer := b.IsGameOver()
-    if gameOver {
-      if winningPlayer == board.Player(-1) {
-        fmt.Println("Game over, it was a draw :D")
-      } else {
-        fmt.Printf("Player %v wins! :D\n", winningPlayer)
-      }
-      break
-    }
-  }
+	fmt.Println("Tic Tac Toe :D")
+	b := board.NewBoard()
+	displayBoard(b)
+	gameOver := false
+	player := 0
+	for !gameOver {
+		player = gameStep(b, player)
+		gameOver, winningPlayer := b.IsGameOver()
+		if gameOver {
+			endGame(b, winningPlayer)
+			break
+		}
+	}
 }
